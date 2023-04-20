6fd402ca14f9e848ebb81c564df11dc06192d537
Frederik Lind <frederik.nic@gmail.com>
2023-04-20 11:27:48 +0200

    Added text reading

--- main.go
+++ main.go
@@ -6,14 +6,43 @@ import (
 	. "github.com/pdftron/pdftron-go/v2"
 )
 
-func main() {
-	PDFNetInitialize("demo:1681479144517:7df72d550300000000cd5af736e910cc63e454cadb25e0d06c2b7147ee")
+const filename = "PDFTRON-test-pdf.pdf"
 
-	filename := "PDFTRON-test-pdf.pdf"
+func main() {
+
+	PDFNetInitialize("demo:1681479144517:7df72d550300000000cd5af736e910cc63e454cadb25e0d06c2b7147ee")
 
 	// open document from the filesystem
 	doc := NewPDFDoc(filename)
 
+	// Printing the filename
 	fmt.Println(doc.GetFileName())
+
+	//Read text from a file example from - https://docs.apryse.com/documentation/linux/guides/features/extraction/text-extract/
+	readTextFromFile()
+
+}
+
+func readTextFromFile() {
+	doc := NewPDFDoc(filename)
+	page := doc.GetPage(1)
+
+	txt := NewTextExtractor()
+	txt.Begin(page) // Readspage
+
+	// Extract words one by one
+	word := NewWord()
+	line := txt.GetFirstLine()
+	for line.IsValid() {
+		word = line.GetFirstWord()
+		for word.IsValid() {
+			// to get words in string you need to call .GetString() on a "word".
+			fmt.Println("This is the current word: ", word.GetString())
+			word = word.GetNextWord()
+		}
+		// To see new lines not needed
+		fmt.Println()
+		line = line.GetNextLine()
+	}
 
 }
