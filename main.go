ad1e78325abb4764e3e8f12770e6c0c0b9f95f78
Frederik Lind <frederik.nic@gmail.com>
2023-04-20 12:00:33 +0200

    Added getting sentences

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 const filename = "PDFTRON-test-pdf.pdf"
+const beeFilename = "bee-movie-script.pdf"
 
 func main() {
 
@@ -20,6 +21,9 @@ func main() {
 
 	//Read text from a file example from - https://docs.apryse.com/documentation/linux/guides/features/extraction/text-extract/
 	readTextFromFile()
+
+	//Read text from a file on a sentence basis.
+	readTextOnASentenceBasis()
 
 }
 
@@ -45,4 +49,44 @@ func readTextFromFile() {
 		line = line.GetNextLine()
 	}
 
+}
+
+func readTextOnASentenceBasis() {
+
+	var sentence string
+	var wordString string
+	var finalCharInWord string
+
+	doc := NewPDFDoc(beeFilename)
+	page := doc.GetPage(1)
+
+	txt := NewTextExtractor()
+	txt.Begin(page) // Readspage
+
+	// Extract words one by one
+	word := NewWord()
+	line := txt.GetFirstLine()
+	for line.IsValid() {
+		word = line.GetFirstWord()
+		for word.IsValid() {
+			// to get words in string you need to call .GetString() on a "word".
+			wordString = word.GetString()
+
+			// Append word to sentence (is is possible to use bytes pgk to do it in O(n) time
+			// https://stackoverflow.com/questions/1760757/how-to-efficiently-concatenate-strings-in-go)
+			sentence += wordString + " "
+
+			// This can probably be done a lot prettier.
+			finalCharInWord = string(wordString[len(wordString)-1])
+
+			// Check to see if there is punctuation to see if there is a sentence.
+			if finalCharInWord == "." || finalCharInWord == "?" || finalCharInWord == "!" {
+				fmt.Println("Final sentence: ", sentence)
+				sentence = ""
+			}
+
+			word = word.GetNextWord()
+		}
+		line = line.GetNextLine()
+	}
 }
